b715e1812521ace8daabdf282e64187060624fe3
Fakhrii Habibillah An Naufal <fha.naufal06@gmail.com>
2022-06-28 07:25:51 +0700

    create obat table migration

---
+++ app/Database/Migrations/2022-06-27-213028_CreateObatTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateObatTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'kode' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'nama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'harga' => [
+                'type' => 'INT',
+            ],
+            'satuan' => [
+                'type' => 'INT',
+            ],
+            'penggunaan_obat' => [
+                'type' => 'TEXT',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME'
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME'
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('obat');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('obat');
+    }
+}
